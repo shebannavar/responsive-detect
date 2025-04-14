5f94431bf5580e9e0e0cfebe72b7f59c175ce6ef
Santosh <santosh.shebannavar@gmail.com>
2025-04-14 14:01:42 +0530

    tests

--- tests/useResponsive.test.js
+++ tests/useResponsive.test.js
@@ -1,13 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vitest_1 = require("vitest");
-const breakpoints_1 = require("../src/breakpoints");
-(0, vitest_1.describe)("getBreakpoint", () => {
-    (0, vitest_1.it)("returns correct breakpoint for various widths", () => {
-        (0, vitest_1.expect)((0, breakpoints_1.getBreakpoint)(400)).toBe("xs");
-        (0, vitest_1.expect)((0, breakpoints_1.getBreakpoint)(600)).toBe("sm");
-        (0, vitest_1.expect)((0, breakpoints_1.getBreakpoint)(800)).toBe("md");
-        (0, vitest_1.expect)((0, breakpoints_1.getBreakpoint)(1000)).toBe("lg");
-        (0, vitest_1.expect)((0, breakpoints_1.getBreakpoint)(1300)).toBe("xl");
-    });
+import { describe, it, expect } from "vitest";
+import { getBreakpoint } from "../src/breakpoints";
+
+describe("getBreakpoint", () => {
+  it("returns correct breakpoint for various widths", () => {
+    expect(getBreakpoint(400)).toBe("xs");
+    expect(getBreakpoint(600)).toBe("sm");
+    expect(getBreakpoint(800)).toBe("md");
+    expect(getBreakpoint(1000)).toBe("lg");
+    expect(getBreakpoint(1300)).toBe("xl");
+  });
 });
